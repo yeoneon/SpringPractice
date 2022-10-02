3f5cfc8b58f4fa063782266fb52466c0d51a742c
yeoneon <yeoneon0524@gmail.com>
2022-10-03 00:28:20 +0900

    feat : 게시글거장 불러오기

---
+++ src/main/java/com/practice/springpractice/domain/posts/Posts.java
@@ -0,0 +1,27 @@
+package com.practice.springpractice.domain.posts;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+}
