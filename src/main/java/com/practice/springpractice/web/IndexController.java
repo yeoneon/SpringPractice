acdac299c98c370f135286ed8d12bc8ec6e24829
yeoneon <yeoneon0524@gmail.com>
2022-10-03 19:31:03 +0900

    feat : 게시글 수정 기능

--- src/main/java/com/practice/springpractice/web/IndexController.java
+++ src/main/java/com/practice/springpractice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.practice.springpractice.web;
 
 import com.practice.springpractice.service.posts.PostsService;
+import com.practice.springpractice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,12 +16,19 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute( "posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/java/com/practice/springpractice/web/PostsApiController.java
+++ src/main/java/com/practice/springpractice/web/PostsApiController.java
@@ -1,8 +1,10 @@
 package com.practice.springpractice.web;
 
 import com.practice.springpractice.service.posts.PostsService;
+import com.practice.springpractice.web.dto.PostsListResponseDto;
 import com.practice.springpractice.web.dto.PostsResponseDto;
 import com.practice.springpractice.web.dto.PostsSaveRequestDto;
+import com.practice.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +21,10 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
